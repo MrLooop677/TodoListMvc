9159b0df774b92a1ced5d8ddfe7acbbde5fe3183
EslamMahmoud <eslammahmoud.div@gmail.com>
2025-10-17 00:54:10 +0300

    update

--- TodoListMvc/DataAccess/ApplicationDbContext.cs
+++ TodoListMvc/DataAccess/ApplicationDbContext.cs
@@ -3,17 +3,11 @@ using TodoListMvc.Models;
 
 namespace TodoListMvc.DataAccess
 {
-
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
         public DbSet<TodoItem> TodoItemes { get; set; }
-
-        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Data Source=PC-46;Initial Catalog=TodoApp;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;");
-        }
-
     }
-
-
 }
--- TodoListMvc/Program.cs
+++ TodoListMvc/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TodoListMvc.DataAccess;
+
 namespace TodoListMvc
 {
     public class Program
@@ -6,8 +9,12 @@ namespace TodoListMvc
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+
             builder.Services.AddControllersWithViews();
+
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer("Data Source=PC-46;Initial Catalog=TodoApp;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;"));
 
             var app = builder.Build();
 
@@ -15,20 +22,19 @@ namespace TodoListMvc
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
